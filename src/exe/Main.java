9d5a840419c28a810631963973cc6a3d7935caf5
Fred <you@example.com>
2021-11-22 16:21:58 +0100

    account table in main created

--- src/exe/Main.java
+++ src/exe/Main.java
@@ -1,17 +1,25 @@
 package exe;
 
+import components.Account;
 import components.Client;
+import components.CurrentAccount;
+import components.SavingsAccount;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
+		int clientNumbers = 3;
 
-		Client[] clients = createArray(3);
+		Client[] clients = createClientArray(clientNumbers);
 		displayClients(clients);
+		
+		Account[] accounts = createAccountArray(clients, clientNumbers*2);
+		displayAccounts(accounts);
 		
 	}
 	
-	static Client[] createArray(int number) {
+	static Client[] createClientArray(int number) {
 		Client[] clients = new Client[number];
 		clients[0] = new Client("Dupont", "Albert");
 		clients[1] = new Client("Le Roux", "Jacques");
@@ -25,4 +33,27 @@ public class Main {
 		}
 	}
 
+	static Account[] createAccountArray(Client[] clients, int number) {
+		Account[] accounts = new Account[number];
+		accounts[0] = new CurrentAccount("Current", clients[0]);
+		accounts[0].setBalance(0.0);
+		accounts[1] = new SavingsAccount("Savings", clients[0]);
+		accounts[1].setBalance(0.0);
+		accounts[2] = new CurrentAccount("Current", clients[1]);
+		accounts[2].setBalance(0.0);
+		accounts[3] = new SavingsAccount("Savings", clients[1]);
+		accounts[3].setBalance(0.0);
+		accounts[4] = new CurrentAccount("Current", clients[2]);
+		accounts[4].setBalance(0.0);
+		accounts[5] = new SavingsAccount("Savings", clients[2]);
+		accounts[5].setBalance(0.0);
+			
+		return accounts;
+	}
+	
+	static void displayAccounts(Account[] accounts) {
+		for (Account a : accounts) {
+			System.out.println(a.toString());
+		}
+	}
 }
