725dae612c9a8a2cadbba78da97a07cc50359917
Fred <you@example.com>
2021-11-22 16:01:46 +0100

    currentaccount and savingsaccount classes created

---
+++ src/components/CurrentAccount.java
@@ -0,0 +1,12 @@
+ // 1.2.2 Creation of the CurrentAccount class
+
+package components;
+
+public class CurrentAccount extends Account{
+
+	public CurrentAccount(String label, Client client) {
+		super(label, client);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
---
+++ src/components/SavingsAccount.java
@@ -0,0 +1,12 @@
+// 1.2.2 Creation of the SavingsAccount class
+
+package components;
+
+public class SavingsAccount extends Account {
+
+	public SavingsAccount(String label, Client client) {
+		super(label, client);
+		// TODO Auto-generated constructor stub
+	}
+
+}
